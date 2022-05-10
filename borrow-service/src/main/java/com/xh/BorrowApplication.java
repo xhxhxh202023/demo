7b43d6d4322a52ca8f254612e75c4cf81e590690
hwj <978709485@qq.com>
2022-05-10 18:32:50 +0800

    hystrix 服务熔断

--- borrow-service/src/main/java/com/xh/BorrowApplication.java
+++ borrow-service/src/main/java/com/xh/BorrowApplication.java
@@ -3,12 +3,14 @@ package com.xh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class BorrowApplication {
 
     public static void main(String[] args) {
--- borrow-service/src/main/java/com/xh/controller/BorrowController.java
+++ borrow-service/src/main/java/com/xh/controller/BorrowController.java
@@ -1,11 +1,14 @@
 package com.xh.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.xh.entity.BorrowDetail;
 import com.xh.service.BorrowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 /**
  * @Auther: xh
@@ -18,8 +21,14 @@ public class BorrowController {
     @Autowired
     BorrowService service;
 
+    @HystrixCommand(fallbackMethod = "wrongPlan")   //hystrix备选方案
     @RequestMapping("/borrow/{uid}")
     BorrowDetail getBorrowDetailByUid(@PathVariable("uid") int uid){
         return service.getBorrowDetailByUid(uid);
+    }
+
+    //hystrix 备选方案
+    BorrowDetail wrongPlan(int uid){
+        return new BorrowDetail(null, Collections.emptyList());
     }
 }

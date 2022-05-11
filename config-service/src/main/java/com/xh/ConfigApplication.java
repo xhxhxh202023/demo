6afd03c00cfd82980625728cd3434bc207d990b1
hwj <978709485@qq.com>
2022-05-11 18:45:51 +0800

    config

---
+++ config-service/src/main/java/com/xh/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.xh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Auther: xh
+ * @Date: 2022/5/11
+ * @Description: com.xh
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
